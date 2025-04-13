c5c9610d0aa4f593269f2608363676047fa93c3b
Dimas Setiaji <dimassetiaji30@gmail.com>
2025-04-13 19:33:34 +0700

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,25 @@ const poppins = Poppins({
 export const metadata: Metadata = {
   title: "Coptera Career",
   description: "Make your dream job come true",
+  icons: {
+    icon: [
+      { url: "/favicon/favicon.ico" },
+      { url: "/favicon/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: [{ url: "/favicon/apple-touch-icon.png" }],
+    other: [
+      {
+        rel: "android-chrome-192x192",
+        url: "/favicon/android-chrome-192x192.png",
+      },
+      {
+        rel: "android-chrome-512x512",
+        url: "/favicon/android-chrome-512x512.png",
+      },
+    ],
+  },
+  manifest: "/favicon/site.webmanifest",
 };
 
 export default function RootLayout({

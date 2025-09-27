f423e3217c57388f6916f6553419b65fa531892f
fathurrobb <ilhamfathurrobbani@gmail.com>
2025-09-27 17:06:49 +0700

    feature: add next-sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,33 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://career.coptera.id",
+  generateRobotsTxt: true,
+  sitemapSize: 5000,
+  changefreq: "daily",
+  priority: 0.7,
+  transform: async (config, path) => {
+    return {
+      loc: path,
+      changefreq: config.changefreq,
+      priority: config.priority,
+      lastmod: new Date().toISOString(),
+    };
+  },
+  additionalPaths: async (config) => {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_COPTERA_API}/api/job-vacancy`
+    );
+    const data = await res.json();
+
+    const jobUrls = data.data.data.map((job) => {
+      return {
+        loc: `/job/${job.id}`,
+        changefreq: "daily",
+        priority: 0.8,
+        lastmod: new Date(job.updated_at || job.created_at).toISOString(),
+      };
+    });
+
+    return jobUrls;
+  },
+};
